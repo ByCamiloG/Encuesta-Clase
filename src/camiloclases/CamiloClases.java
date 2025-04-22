ca335b2d8ea883299d60cddfe8adeb38f4495cf9
Camila Tautiva <mtautivag@unal.edu.co>
2025-04-22 15:59:37 -0500

    V2

--- src/camiloclases/CamiloClases.java
+++ src/camiloclases/CamiloClases.java
@@ -17,9 +17,19 @@ public class CamiloClases {
     public static void main(String[] args) {
         
     Scanner scanner = new Scanner(System.in); //leer teclado 
-    String nombre = scanner.nextString();
-    
-    
+    System.out.print("Por favor ingrese su nombre: ");
+    String nombre = scanner.nextLine(); 
+    var ascii_code = nombre.charAt(0); 
+    System.out.print("Su nombre es " + nombre);
+      
+
+    System.out.print("Por favor ingrese su edad: ");
+    Integer edad = scanner.nextInt(); 
+    Integer edadInteger = edad; //
+    System.out.print("Su edad es " + edadInteger);
     }
     
+    /*var x = 'B';
+var ascii_code = x.charCodeAt(0);
+console.log(ascii_code);*/
 }
