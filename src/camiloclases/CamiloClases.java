526cc0f3e36d8746da2f24ff0489a3631071dc3c
Camila Tautiva <mtautivag@unal.edu.co>
2025-04-22 15:23:00 -0500

    V1

---
+++ src/camiloclases/CamiloClases.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package camiloclases;
+import java.util.Scanner;
+import java.math.BigInteger;
+/**
+ *
+ * @author Estudiante
+ */
+public class CamiloClases {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    Scanner scanner = new Scanner(System.in); //leer teclado 
+    String nombre = scanner.nextString();
+    
+    
+    }
+    
+}
